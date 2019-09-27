d423b5ff6119fe935c2981ca71ac6aa2fd2fcd1e
Erik Malm <malm.erik@gmail.com>
2019-09-27 16:37:06 +0200

    Added some example callbacks and prop propagation for learning

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,24 +1,27 @@
 import React,{ Component } from 'react';
 import './app.css';
 import ReactImage from './react.png';
+import Comp from "./Comp.jsx"
 
 export default class App extends Component {
 
 state = {simple:null}
 componentWillmount() {
-  this.setState({simple:"Not gotten"});
+    this.setState({simple:"Not gotten"});
   }
   componentDidMount() {
+    this.setState({propOne:this.props.propOne});
     fetch('/api/pomodoro')
       .then(res => res.json())
       .then(simple => this.setState({ simple: simple.simple }));
+    setTimeout(() => { this.setState({propOne:"GOODBYE"})},5000);
   }
 
   render() {
-    const { simple } = this.state;
     return (
       <div>
-        {simple ? <h1>{`Hello ${simple}`}</h1> : <h1>Updating the page when fetched.</h1>}
+        <h1>{this.props.propOne}</h1>
+        <Comp propOne={this.state.propOne} callback={() => { this.setState({propOne:"I WAS CALLED BACK"})}}/>
       </div>
     );
   }
